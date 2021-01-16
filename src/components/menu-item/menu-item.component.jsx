b3fa627df32666645bc03cc94919b2e9b51410dd
Nikola0707 <ristoski.nikola@gmail.com>
2021-01-16 22:10:12 +0100

    update our menu-item to navigatge withRouter HOC(Higher order component)

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,11 +1,16 @@
 import React from "react";
+import { withRouter } from 'react-router-dom';
+
 import "./menu-item-styles.scss";
 
-const MenuItem = ({ title, imageUrl, size }) => {
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => {
+// title, imageUrl, size and linkUrl is coming from directory.component.jsx 
+// history is coming from export default withRouter(MenuItem);
   return (
     <div
       //   if props size == large add className large and className menu-item, else only className menu-item
       className={`${size} menu-item`}
+      onClick= {() => history.push(`${match.url}${linkUrl}`)}   
     >
       <div
         className="background-image"
@@ -22,4 +27,4 @@ const MenuItem = ({ title, imageUrl, size }) => {
   );
 };
 
-export default MenuItem;
+export default withRouter(MenuItem);
