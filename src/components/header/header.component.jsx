02863731dfb22e297810a8a24799d5bec8c06437
Nikola0707 <ristoski.nikola@gmail.com>
2021-01-24 14:43:01 +0100

    finishing sign in handleSubmit method

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 // auth library from firebase utils
 import { auth } from '../../firebase/firebase.utils'
